7ca416f0e65892ae4fb1ec66afb3f0b4d9935b09
Ignat Rybakov <rybignat389@gmail.com>
2024-11-24 16:20:59 +0300

    improve workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,10 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true, // Запуск тестов в параллельном режиме
+  fullyParallel: false, // Запуск тестов в параллельном режиме
   forbidOnly: !!process.env.CI, // Запрет использования test.only в CI
   retries: process.env.CI ? 2 : 0, // Повторные попытки в CI
-  workers: process.env.CI ? 1 : 1, // Количество воркеров в CI
+  workers: process.env.CI ? 4 : 4, // Количество воркеров в CI
   reportSlowTests: null,
   reporter: [['list'], ['./Utils/customReporter.ts']], // Используемый репортер
 
